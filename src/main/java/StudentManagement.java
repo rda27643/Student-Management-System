6c77455b779a4ee8ccf8d988b906c7142fa078c4
Reza Abbasi <rda27643@gmail.com>
2026-08-03 15:59:24 +0330

    feat: show all students adding

--- src/main/java/StudentManagement.java
+++ src/main/java/StudentManagement.java
@@ -9,4 +9,15 @@ public class StudentManagement {
     public void addStudent(Student student){
         students.add(student);
     }
+    public void showAllStudents(){
+        int count = 0;
+        if (students.isEmpty()){
+            System.out.println("no student for show!");
+        } else {
+            for (Student s : students) {
+                System.out.println("S" + count++);
+                System.out.println("\t" + s);
+            }
+        }
+    }
 }
