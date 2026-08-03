002ac7230d7a69163ba2e21476c842af6b459b86
Reza Abbasi <rda27643@gmail.com>
2026-08-03 12:08:22 +0330

    feat: add cunstructore

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,16 @@
 public class Student {
+    private String name;
+    private String email;
+    private Major major;
+    private String age;
+    private String id;
+    private static int counter = 0;
+
+    public Student(String name, String email, Major major, String age) {
+        this.id = "405" + counter++;
+        this.name = setName(name);
+        this.email = setEmail(email);
+        this.major = setMajor(major);
+        this.age = setAge(age);
+    }
 }
