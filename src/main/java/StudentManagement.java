7ad6c9e885cbff6c4a83a76eadbd35bc6762e2de
Reza Abbasi <rda27643@gmail.com>
2026-08-03 20:39:54 +0330

    feat: add search method

--- src/main/java/StudentManagement.java
+++ src/main/java/StudentManagement.java
@@ -19,7 +19,16 @@ public class StudentManagement {
         int counter = 0;
         for (Map.Entry<String, Student> i : students.entrySet()) {
             System.out.println("S " +counter + ":");
-            System.out.println("\t" + i.getValue());
+            System.out.println(i.getValue());
         }
     }
+    public Student searchStudent(String studNumber){
+        if (students.get(studNumber) == null){
+            return null;
+        } else {
+            return students.get(studNumber);
+        }
+    }
+
+
 }
