1b061d6b6cef82ccd90b6abe9987cb84a9fdbc9a
Reza Abbasi <rda27643@gmail.com>
2026-08-03 15:46:41 +0330

    feat: add student method

--- src/main/java/StudentManagement.java
+++ src/main/java/StudentManagement.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class StudentManagement {
+    private ArrayList<Student> students;
+
+    public StudentManagement() {
+        this.students = new ArrayList<>();
+    }
+    public void addStudent(Student student){
+        students.add(student);
+    }
 }
