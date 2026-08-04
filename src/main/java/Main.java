06089170860c68f136c96ca1572f5d8a07600ea6
Reza Abbasi <rda27643@gmail.com>
2026-08-04 23:16:34 +0330

    feat: add display menu method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+
+    }
+    public void displayMenu(){
+        String[] menu = {"Add Student" , "Show Student" , "Search Student" , "Edit Student" , "Delete Student" , "Exit"};
+        int counter = 1;
+        System.out.println("====== Menu ======");
+        for (String s : menu) {
+            System.out.println(counter + ". " + s);
+        }
+        System.out.println("============");
+    }
 }
